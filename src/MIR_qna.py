4193daf877fa6b1d6809e101c81ea74748f0f6f3
SimplyJuanjo <juanjodoolmo@gmail.com>
2024-09-16 12:15:40 +0200

    update MIR gpt4o y c35sonnet

--- src/MIR_qna.py
+++ src/MIR_qna.py
@@ -1,15 +1,30 @@
 import os
 import pandas as pd
 from dotenv import load_dotenv
-from langchain.chat_models import AzureChatOpenAI
+from langchain.chat_models import AzureChatOpenAI, ChatOpenAI
 from langchain.schema import HumanMessage
 from langchain.prompts import PromptTemplate
 from langchain.prompts.chat import ChatPromptTemplate, SystemMessagePromptTemplate, HumanMessagePromptTemplate
 from tqdm import tqdm
+import anthropic
 
 
 # Load the environment variables from the .env file
 load_dotenv()
+
+def initialize_anthropic_c35(prompt, temperature=0, max_tokens=2000):
+    client = anthropic.Anthropic(
+        # defaults to os.environ.get("ANTHROPIC_API_KEY")
+        api_key=os.environ.get("ANTHROPIC_API_KEY")
+    )
+    message = client.messages.create(
+        model="claude-3-5-sonnet-20240620",
+        max_tokens=max_tokens,
+        temperature=temperature,
+        messages=[{"role": "user", "content": prompt}]
+    )
+    # print(message.content)
+    return message
 
 # Initialize the AzureChatOpenAI model
 model = AzureChatOpenAI(
@@ -19,6 +34,16 @@ model = AzureChatOpenAI(
     max_tokens=800,
     model_kwargs={"top_p": 1, "frequency_penalty": 0, "presence_penalty": 0}
 )
+
+model_name = "gpt-4o"
+openai_api_key=os.getenv("OPENAI_API_KEY")
+gpt4o = ChatOpenAI(
+        openai_api_key = openai_api_key,
+        model_name = model_name,
+        temperature = 0,
+        max_tokens = 800,
+    )
+
 
 # Importing the dataset from MIR md
 dataset = pd.read_excel('data/MIR24GPT.xlsx')
@@ -39,17 +64,22 @@ for index, row in tqdm(dataset.iterrows(), total=dataset.shape[0]):
     attempts = 0
     while attempts < 2:
         try:
-            diagnosis = model(formatted_prompt).content  # Call the model instance directly
+            diagnosis = gpt4o(formatted_prompt).content  # Call the model instance directly
+            print(diagnosis)
+            diagnosis_c35 = initialize_anthropic_c35(formatted_prompt[0].content).content[0].text
+            print(diagnosis_c35)
             break
         except Exception as e:
             attempts += 1
             print(e)
             if attempts == 2:
                 diagnosis = "ERROR"
+                diagnosis_c35 = "ERROR"
 
     # Add the diagnoses to column number 6 (GPT-4)
     dataset.iloc[index, 5] = diagnosis
+    dataset.iloc[index, 6] = diagnosis_c35
 
 # Save the diagnoses to a new XLSX file
-dataset.to_excel('data/MIR24GPT_answered.xlsx', index=False)
+dataset.to_excel('data/MIR24GPT_answered_20240916.xlsx', index=False)
 

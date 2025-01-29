27beef86e16be1bc3d0c2713640ef0257af3a9dd
SimplyJuanjo <juanjodoolmo@gmail.com>
2025-01-29 18:25:01 +0100

    update_2025, added o1 full model

--- src/MIR_qna.py
+++ src/MIR_qna.py
@@ -28,10 +28,10 @@ def initialize_anthropic_c35(prompt, temperature=0, max_tokens=2000):
 
 # Initialize the AzureChatOpenAI model
 model = AzureChatOpenAI(
-    openai_api_version="2023-06-01-preview",
-    azure_deployment="nav29",
-    temperature=0,
-    max_tokens=800,
+    openai_api_version="2024-12-01-preview",
+    azure_deployment="o1",
+    temperature=1,
+    max_completion_tokens=800,
     model_kwargs={"top_p": 1, "frequency_penalty": 0, "presence_penalty": 0}
 )
 
@@ -46,40 +46,47 @@ gpt4o = ChatOpenAI(
 
 
 # Importing the dataset from MIR md
-dataset = pd.read_excel('data/MIR24GPT.xlsx')
+# dataset = pd.read_excel('data/MIR24GPT.xlsx')
 
+dataset2025 = pd.read_excel('data/MIR25_answered_20250128.xlsx', sheet_name='Sheet1')
+print(dataset2025.columns)
 # In col, GPT-4, save the answers from GPT-4
 PROMPT_TEMPLATE = "Behave like a hypotethical doctor who has to answer the following question. You have to indicate only a number 1-4 with the correct answer. Don't output anything different than 1-4 please. The question is \n:{description}"
+
+PROMPT_2025 = "Behave like a hypotethical doctor who has to answer the following question. I am taking the Spanish MIR exam. Answer only the correct answer, there is no need to explain the answer (there can only be one correct): \n {description}"
 
 # Define the chat prompt templates
 human_message_prompt = HumanMessagePromptTemplate.from_template(PROMPT_TEMPLATE)
 chat_prompt = ChatPromptTemplate.from_messages([human_message_prompt])
 
 # Iterate over the rows with indices 0-10 in the data
-for index, row in tqdm(dataset.iterrows(), total=dataset.shape[0]):
+for index, row in tqdm(dataset2025.iterrows(), total=dataset2025.shape[0]):
     # Get the ground truth (GT) and the description
-    description = row[1]
+    description = row["Description"]
+    print(f"Description: {description}")
     # Generate the answer using the GPT-4 model
     formatted_prompt = chat_prompt.format_messages(description=description)
     attempts = 0
     while attempts < 2:
         try:
-            diagnosis = gpt4o(formatted_prompt).content  # Call the model instance directly
-            print(diagnosis)
-            diagnosis_c35 = initialize_anthropic_c35(formatted_prompt[0].content).content[0].text
-            print(diagnosis_c35)
+            # diagnosis = gpt4o(formatted_prompt).content  # Call the model instance directly
+            # print(diagnosis)
+            # diagnosis_c35 = initialize_anthropic_c35(formatted_prompt[0].content).content[0].text
+            # print(diagnosis_c35)
+            diagnosis = model.invoke(formatted_prompt)
+            print(diagnosis.content)
             break
         except Exception as e:
             attempts += 1
             print(e)
             if attempts == 2:
                 diagnosis = "ERROR"
-                diagnosis_c35 = "ERROR"
+                # diagnosis_c35 = "ERROR"
 
     # Add the diagnoses to column number 6 (GPT-4)
-    dataset.iloc[index, 5] = diagnosis
-    dataset.iloc[index, 6] = diagnosis_c35
+    dataset2025.iloc[index, 5] = diagnosis.content
+    # dataset2025.iloc[index, 4] = diagnosis_c35
 
 # Save the diagnoses to a new XLSX file
-dataset.to_excel('data/MIR24GPT_answered_20240916.xlsx', index=False)
+dataset2025.to_excel('data/MIR25_answered_20250128.xlsx', index=False)
 
